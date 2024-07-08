26d4cd965a655d8debfb22b5111803824f66a1b7
aburaihan8989 <yusup.smartphone@gmail.com>
2024-07-08 22:20:21 +0700

    update

--- Modules/Manifest/Resources/views/umroh/partials/status-customer.blade.php
+++
@@ -1,9 +0,0 @@
-@if ($data->status == 'Completed')
-    <span class="badge badge-success" style="font-size: 13px;">
-        {{ $data->status }}
-    </span>
-@else
-    <span class="badge badge-danger" style="font-size: 13px;">
-        {{ $data->status }}
-    </span>
-@endif
--- Modules/Manifest/Resources/views/umroh/payments/partials/status.blade.php
+++
@@ -1,9 +0,0 @@
-@if ($data->status == 'Verified')
-    <span class="badge badge-success" style="font-size: 13px;">
-        {{ $data->status }}
-    </span>
-@else
-    <span class="badge badge-danger" style="font-size: 13px;">
-        {{ $data->status }}
-    </span>
-@endif
