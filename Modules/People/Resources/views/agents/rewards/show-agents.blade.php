9deb36c637df49a49e56b3de1a33b59c5e859cd5
aburaihan8989 <yusup.smartphone@gmail.com>
2024-07-10 09:09:55 +0700

    update

--- Modules/People/Resources/views/agents/rewards/show-agents.blade.php
+++ Modules/People/Resources/views/agents/rewards/show-agents.blade.php
@@ -63,15 +63,15 @@
                                                     <button type="button" class="btn btn-ghost-primary dropdown rounded" data-toggle="dropdown" aria-expanded="false">
                                                         <i class="bi bi-three-dots-vertical"></i>
                                                     </button>
-                                                    <div class="dropdown-menu">
+                                                    {{-- <div class="dropdown-menu"> --}}
                                                         {{-- <a href="{{ route('rewards-customers-referal-list.show-customers', $data->id) }}" class="dropdown-item"> --}}
-                                                        <a href="#" class="dropdown-item text-secondary">
+                                                        {{-- <a href="#" class="dropdown-item text-secondary">
                                                             <i class="bi bi-people-fill mr-2 text-secondary" style="line-height: 1;"></i> Show Customers
-                                                        </a>
-                                                        <a href="#" class="dropdown-item text-secondary">
+                                                        </a> --}}
+                                                        {{-- <a href="#" class="dropdown-item text-secondary">
                                                             <i class="bi bi-eye mr-2 text-secondary" style="line-height: 1;"></i> Details
-                                                        </a>
-                                                    </div>
+                                                        </a> --}}
+                                                    {{-- </div> --}}
                                                 </div>
                                             </td>
                                         </tr>
