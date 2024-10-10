3c3c5810bbce1a0b8671ec2055f864a1f5c7fd0a
aburaihan8989 <yusup.smartphone@gmail.com>
2024-10-10 18:56:41 +0700

    update

--- Modules/Report/Resources/views/report1/partials/actions-pagi.blade.php
+++ Modules/Report/Resources/views/report1/partials/actions-pagi.blade.php
@@ -1,5 +1,5 @@
 {{-- @can('show_products') --}}
-<a href="{{ route('report1result.pagi_create', $data->id) }}" class="btn btn-primary" style="width:90px; font-size:14px">
+<a href="{{ route('report1result.pagi_create', $data->id) }}" class="btn btn-primary" style="width:100px; font-size:14px">
     Nilai Pagi
 </a>
 {{-- @endcan --}}
--- Modules/Report/Resources/views/report1/partials/actions-siang.blade.php
+++ Modules/Report/Resources/views/report1/partials/actions-siang.blade.php
@@ -1,5 +1,5 @@
 {{-- @can('show_products') --}}
-<a href="{{ route('report1result.siang_create', $data->id) }}" class="btn btn-primary" style="width:90px; font-size:14px">
+<a href="{{ route('report1result.siang_create', $data->id) }}" class="btn btn-primary" style="width:100px; font-size:14px">
     Nilai Siang
 </a>
 {{-- @endcan --}}
